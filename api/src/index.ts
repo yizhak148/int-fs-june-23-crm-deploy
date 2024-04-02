9e0988639b95d159d676a1725155d72bf619acc8
Lilach E <lilachelayza@gmail.com>
2024-04-02 15:35:44 +0300

    add stub: POST /registerLead

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,13 +11,26 @@ app.use(cors());
 app.use(json());
 
 app.get("/check", async (_, res) => {
-    res.status(200);
-    res.json({ status: "OK" });
+  res.status(200);
+  res.json({ status: "OK" });
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    // add POST request code
+
+    res.status(201);
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+    res.json({ error: "something went wrong" });
+  }
 });
 
 async function init() {
-    await initConnection();
-    app.listen(3000, () => console.log("Server running on localhost:3000"));
+  await initConnection();
+  app.listen(3000, () => console.log("Server running on localhost:3000"));
 }
 
 init();
