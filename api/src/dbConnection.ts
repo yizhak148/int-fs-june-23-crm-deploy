339aeccef113ae70b02a3e3df78f0e8819032c2f
Assaf Basri <Faskoman90@gmail.com>
2024-04-08 19:42:26 +0300

    added filters

--- api/src/dbConnection.ts
+++ api/src/dbConnection.ts
@@ -1,20 +1,21 @@
+import "dotenv/config";
 import { createConnection, Connection } from "mysql2/promise";
 
 let connection: Connection | undefined;
 
 export function getConnection() {
-    if (!connection) {
-        throw new Error("Must init connection first!");
-    }
+  if (!connection) {
+    throw new Error("Must init connection first!");
+  }
 
-    return connection;
+  return connection;
 }
 
 export async function initConnection() {
-    connection = await createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: "sql11696756"
-    });
+  connection = await createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "sql11696756",
+  });
 }
--- app/src/LeadsPage.tsx
+++ app/src/LeadsPage.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import "./LeadsPage.scss";
 import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const server = axios.create({
   baseURL: "http://localhost:5173",
@@ -32,11 +33,14 @@ interface Lead {
 export function LeadsPage() {
   const [leads, setLeads] = useState<Lead[]>([]);
   const [search, setSearch] = useState<string>("");
+  // const [searchParams] = useSearchParams();
 
   useEffect(() => {
     const fetchLeads = async () => {
       try {
-        const response = await server.get(`http://localhost:3000/leads?search=${search}`);
+        const response = await server.get(
+          `http://localhost:3000/leads?search=${search}`
+        );
         setLeads(response.data);
       } catch (err) {
         console.error(err);
@@ -49,6 +53,29 @@ export function LeadsPage() {
   return (
     <div>
       <h1>Browse Leads</h1>
+      <menu  className="filter">
+        <div>
+          <label htmlFor="priority">Priority</label>
+          <select name="priority" id="priority">
+          <option value="all">All</option>
+            <option value="low">Low</option>
+            <option value="medium">Medium</option>
+            <option value="high">High</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="stage">Stage</label>
+          <select name="stage" id="stage">
+            <option value="all">All</option>
+            <option value="new">New</option>
+            <option value="acknowledged">Acknowledged</option>
+            <option value="negotiation">Negotiation</option>
+            <option value="contractSent">Contract Sent</option>
+            <option value="customer">Customer</option>
+            <option value="closed">Closed</option>
+          </select>
+        </div>
+      </menu>
       <input
         type="text"
         value={search}
@@ -63,16 +90,22 @@ export function LeadsPage() {
             <th>Phone Number</th>
             <th>Priority</th>
             <th>Stage</th>
+            <th>Created at</th>
           </tr>
         </thead>
         <tbody>
           {leads.map((lead, index) => (
             <tr key={index} className="leadsTable__leadInfo">
-              <td className="leadsTable__leadInfo__name">{lead.firstName} {lead.lastName}</td>
+              <td className="leadsTable__leadInfo__name">
+                <a href={`leads/${lead.id}`}>
+                  {lead.firstName} {lead.lastName}
+                </a>
+              </td>
               <td>{lead.email}</td>
               <td>{lead.phoneNumber}</td>
               <td>{lead.priority}</td>
               <td>{lead.stage}</td>
+              <td>{lead.createdAt}</td>
             </tr>
           ))}
         </tbody>
